cddf15c9dbd79263ee4c26cd534fc49ca45246ab
Anthon002 <chineduanulugwo@gmail.com>
2024-03-07 08:20:50 -0800

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,23 +5,16 @@ This module contains the pascal triangle
 
 
 def pascal_triangle(n):
-    '''
-    This is the pascal triangle implementation
-    '''
-    if (n <= 0):
-        return []
     greaterList = []
+    if (n <= 0):
+        return greaterList
     for i in range(n):
-        pascalArray = []
+        pascalArray = [1]
         val = 1
         for j in range(i):
             val = val *(i-j)/(j+1)
             pascalArray.append(int(val))
-        if (len(pascalArray) > 0):
-            if (len(pascalArray) == 1):
-                greaterList.append(pascalArray)
-                greaterList.append([1,1])
-            else:
-                greaterList.append([1]+pascalArray)
-            
+
+        greaterList.append(pascalArray)
+
     return greaterList;
