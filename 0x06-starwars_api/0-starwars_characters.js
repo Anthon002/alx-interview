d143aa5bc2d3f66846e420d12463c5987a689b3e
anthon002 <chineduanulugwo@gmail.com>
2024-04-18 20:42:08 +0000

    reverting

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
-const API_URL = 'https://swapi-api.hbtn.io/api';
+const URL_API = 'https://swapi-api.hbtn.io/api';
 
 if (process.argv.length > 2) {
-  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+  request(`${URL_API}/films/${process.argv[2]}/`, (err, _, body) => {
     if (err) {
       console.log(err);
     }
-    const charactersURL = JSON.parse(body).characters;
-    const charactersName = charactersURL.map(
+    const charURL = JSON.parse(body).characters;
+    const charName = charURL.map(
       url => new Promise((resolve, reject) => {
         request(url, (promiseErr, __, charactersReqBody) => {
           if (promiseErr) {
@@ -18,7 +18,7 @@ if (process.argv.length > 2) {
         });
       }));
 
-    Promise.all(charactersName)
+    Promise.all(charName)
       .then(names => console.log(names.join('\n')))
       .catch(allErr => console.log(allErr));
   });
