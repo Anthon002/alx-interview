1fecdfcc17e18bf5b65290b346d4726610d5df4d
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2024-04-18 20:50:22 +0100

    task complete

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
-const requests_ = require('request');
+const request = require('request');
 const URL_API = 'https://swapi-api.hbtn.io/api';
 
 if (process.argv.length > 2) {
-  requests_(`${URL_API}/films/${process.argv[2]}/`, (err, _, body) => {
+  request(`${URL_API}/films/${process.argv[2]}/`, (err, _, body) => {
     if (err) {
       console.log(err);
     }
     const charURL = JSON.parse(body).characters;
     const charName = charURL.map(
       url => new Promise((resolve, reject) => {
-        requests_(url, (promiseErr, __, charactersReqBody) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
           if (promiseErr) {
             reject(promiseErr);
           }
