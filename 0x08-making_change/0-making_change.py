4a392c3e58dfe66d74094a9adfc0000642956954
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2024-05-02 22:47:04 +0100

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
-"""Change making module.
+"""module for making change
 """
 
 
 def makeChange(coins, total):
-    """Determines the fewest number of coins needed to meet a given
-    amount total when given a pile of coins of different values.
+    """ method to Determines the fewest number of coins that is needed to reach a change amount.
     """
     if total <= 0:
         return 0
     rem = total
-    coins_count = 0
-    coin_idx = 0
+    count_coin = 0
+    idxcoin_ = 0
     sorted_coins = sorted(coins, reverse=True)
     n = len(coins)
     while rem > 0:
-        if coin_idx >= n:
+        if idxcoin_ >= n:
             return -1
-        if rem - sorted_coins[coin_idx] >= 0:
-            rem -= sorted_coins[coin_idx]
-            coins_count += 1
+        if rem - sorted_coins[idxcoin_] >= 0:
+            rem -= sorted_coins[idxcoin_]
+            count_coin += 1
         else:
-            coin_idx += 1
-    return coins_count
+            idxcoin_ += 1
+    return count_coin
