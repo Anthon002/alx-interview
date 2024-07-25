823be76c8da429f8d3096a462a1484ddad940c3a
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2024-07-25 14:15:54 +0100

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
-"""Change making module.
+"""Currency exchange module.
 """
 
-
 def makeChange(coins, total):
-    """Determines the fewest number of coins needed to meet a given
-    amount total when given a pile of coins of different values.
+    """Determines the fewest number of currency units needed to meet a given
+    total when given a collection of different denomination values.
     """
     if total <= 0:
         return 0
-    rem = total
-    coins_count = 0
-    coin_idx = 0
+    remaining = total
+    units_count = 0
+    denom_idx = 0
     sorted_coins = sorted(coins, reverse=True)
-    n = len(coins)
-    while rem > 0:
-        if coin_idx >= n:
+    num_denoms = len(coins)
+    while remaining > 0:
+        if denom_idx >= num_denoms:
             return -1
-        if rem - sorted_coins[coin_idx] >= 0:
-            rem -= sorted_coins[coin_idx]
-            coins_count += 1
+        if remaining - sorted_coins[denom_idx] >= 0:
+            remaining -= sorted_coins[denom_idx]
+            units_count += 1
         else:
-            coin_idx += 1
-    return coins_count
+            denom_idx += 1
+    return units_count
